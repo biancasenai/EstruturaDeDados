ff7759b29ed2e74947b4dd73c3d3df96ee08cd07
Bianca Furlanetto <bianca.furlanetto440@gmail.com>
2024-08-21 15:31:21 -0300

    finalizando exemplo de dicionario e de dados

--- EstruturaDeDados/Program.cs
+++ EstruturaDeDados/Program.cs
@@ -2,6 +2,92 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //TRABALHANDO COM LISTA (LIST)
+        List<string> frutas = new List<string>();
+
+        // Adicionar itens na Lista
+        frutas.Add("Morango");
+        frutas.Add("Manga");
+        frutas.Add("Goiaba");
+        frutas.Add("Uva");
+
+        // Imprimir os itens da lista
+        Console.WriteLine("Minha Lista de frutas");
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+
+        Console.WriteLine("Fruta na indice 2: " + frutas[2]);
+
+        /* inserir um elemento na posição especifico */
+        frutas.Insert(1, "Maracujá");
+
+        // imprimindo a lista novamente 
+        Console.WriteLine("Minha Lista de frutas");// pula linha em branco
+
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+
+        //alterar um elemento no indice especifico
+        frutas[4] = "Pêra";
+        Console.WriteLine("Minha Lista de frutas");// pula linha em branco
+
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+
+        // remover elementos da lista no indice especifico
+        frutas.RemoveAt(3);
+        //remover elementos pelo valor do conteudo
+        frutas.Remove("Morango");
+
+        // imprimindo a lista novamente
+        Console.WriteLine(); //pula linha em branco
+        Console.WriteLine("Minha Lista de frutas");// pula linha em branco
+        frutas.ForEach(Console.WriteLine);
+        {
+
+        }
+
+        //apagar todos os elementos da lista
+        frutas.Clear();
+
+        //TRABALHANDO COM DICIONARIO ( DICTIONARY)
+
+        /* criando um dicionario de dados */
+        Dictionary<int, string> carros = new Dictionary<int, string>();
+        /* adicionar dados a um dicionario */
+        carros.Add(5, "Corsa");
+        carros.Add(10, "Fusca");
+        carros.Add(2, "Ford Ka");
+
+
+        Console.WriteLine(); //pula linha em branco
+        Console.WriteLine("===========================");
+        Console.WriteLine(); //pula linha em branco
+
+        Console.WriteLine("Meu dicionario de carros:");
+        /* imprimir um dicionario de dados*/
+        foreach (var carro in carros)
+        {
+            Console.WriteLine($"{carro.Key} - {carro.Value}");
+
+
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
